991515c6ed06cd2f04192e6086799dcd0f0731eb
gianlucaKun <gianlucakun95@gmail.com>
2025-01-09 19:43:33 +0100

    test lorem before section

--- frontend/elektrallarmfe/src/pages/home/Home.jsx
+++ frontend/elektrallarmfe/src/pages/home/Home.jsx
@@ -12,7 +12,23 @@ const Home = () => {
       <BioHome/>
       <Carosello />
       
-     
+     <div><div className="containerContactMe padding20">
+        <h2>
+          Elektrallarm: innovazione e sicurezza per la tua casa o attività.
+        </h2>
+        <h2>{" "}test</h2>
+      </div>
+      <div>
+        <h1>form contattami</h1>
+      </div><div className="containerContactMe padding20">
+        <h2>
+          Elektrallarm: innovazione e sicurezza per la tua casa o attività.
+        </h2>
+        <h2>{" "}test</h2>
+      </div>
+      <div>
+        <h1>form contattami</h1>
+      </div></div>
       <SectionServices />
       <div className="containerContactMe padding20">
         <h2>
